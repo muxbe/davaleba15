458a511375477e07a02539084e5fbd2823082dda
muxbe <159569051+muxbe@users.noreply.github.com>
2024-04-07 13:45:26 +0200

    damatebiti1

--- new.js
+++ new.js
@@ -29,6 +29,44 @@ let time = currentDate.getHours();
 let monthday = currentDate.getDate();
 let minut = currentDate.getMinutes();
 let second = currentDate.getSeconds();
+switch (month) {
+  case 0:
+    month = "January";
+    break;
+  case 1:
+    month = "February";
+    break;
+  case 2:
+    month = "March";
+    break;
+  case 3:
+    month = "April";
+    break;
+  case 4:
+    month = "May";
+    break;
+  case 5:
+    month = "June";
+    break;
+  case 6:
+    month = "July";
+    break;
+  case 7:
+    month = "August";
+    break;
+  case 8:
+    month = "September";
+    break;
+  case 9:
+    month = "October";
+    break;
+  case 10:
+    month = "November";
+    break;
+  case 11:
+    month = "December";
+    break;
+}
 console.log(
   year + "/" + month + "/" + monthday + "/" + time + ":" + minut + ":" + second
 );
@@ -62,3 +100,49 @@ switch (day) {
     console.log("today is ", day);
     break;
 }
+let price = 100;
+let delivery = 20;
+
+let city = "batumi";
+switch (city) {
+  case "tbilisi":
+    delivery = 0;
+    console.log("free delivery");
+    break;
+  case "batumi":
+  case "kutaisi":
+  case "rustavi":
+    delivery = 10;
+    console.log("half price for delivery");
+    break;
+  default:
+    delivery = 30;
+    console.log("from your region delivery is very hard");
+}
+let totalSum = 200;
+switch (totalSum) {
+  case 0:
+    totalSum = 0;
+    delivery = 0;
+    console.log("total sum-", totalSum);
+    break;
+  case 100:
+    totalSum = 100;
+    console.log(totalSum);
+    break;
+  case 200:
+    i = 5 / 200;
+    i *= 100;
+    totalSum -= i;
+    console.log(totalSum, "GEL", " save 5%");
+    break;
+  case 300:
+    i = 10 / 300;
+    i *= 100;
+    totalSum -= i;
+    console.log(totalSum, "GEL", "save 10%");
+    break;
+}
+
+let totalprice = totalSum + delivery;
+console.log("total price-", totalprice, "GEL");
